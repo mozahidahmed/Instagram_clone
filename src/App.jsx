4fb988355c44eca797b2e9cb2373aeb39e8b894a
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-11-22 21:56:23 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Login from './components/Authentication/Login';
+import SignUp from './components/Authentication/SignUp';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+
+const App = () => {
+  return (
+    <div className="bg-[#020617]">
+      
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/signin" element={<Login />}></Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
