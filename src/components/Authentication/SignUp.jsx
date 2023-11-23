ae36ebaf8a921b3eeaf3440af5b95a553ad35ac1
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-11-23 13:43:51 +0600

    some'

--- src/components/Authentication/SignUp.jsx
+++ src/components/Authentication/SignUp.jsx
@@ -36,7 +36,7 @@ const SignUp = () => {
 
     return (
       <div>
-        <section className="">
+        <section className="h-screen grid justify-center items-center">
           <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
             <div className="w-full bg-gradient-to-r from-pink-300  to-pink-500 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
               <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
